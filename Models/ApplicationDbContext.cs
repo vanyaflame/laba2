10a0e6181687d943970680bc139c566643c2d74a
Danil I <vanyaflame228@gmail.com>
2025-09-26 17:17:11 +0300

    go grow never stop

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using SimpleTaskApp.Models;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace SimpleTaskApp.Models
 {
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Конфигурация подключения к БД
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-    ?? "Host=db;Database=taskdb;Username=postgres;Password=YourStrongPassword123";
+    ?? "Host=db;Database=labdb;Username=student;Password=student123";
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
